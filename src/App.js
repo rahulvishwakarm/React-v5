40d5be3813e2df28c3368cd0be2a587465891a3f
rahulvishwakarm <init830@gmail.com>
2021-02-25 15:54:14 +0530

    Parcel and ImportReact and ReactDom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import {render} from 'react-dom'
+
 const Pet = ({name,rollno}) =>{
     return React.createElement("div",{},[
         React.createElement("h1",{},name),
@@ -17,7 +20,7 @@ const App = () =>{
         );
 };
 
-ReactDOM.render(
+render(
     React.createElement(App),
     document.getElementById("root")
 );

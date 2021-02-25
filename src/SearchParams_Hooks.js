c9f4321ca178d04ba6d86506dacd36a28fce6a17
rahulvishwakarm <init830@gmail.com>
2021-02-25 17:40:54 +0530

    useState Hook

--- src/SearchParams_Hooks.js
+++ src/SearchParams_Hooks.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const SearchParams_Hooks=()=> {
 
-    const location = "Mumbai,Mahrashtra";
+    // const location = "Mumbai,Mahrashtra";
+
+    const [location,setLocation] = useState("Mumbai,Mahrashtra"); //Hooks start with use
+
     return (
         <div className="searchparams">
+            <h1>{location}</h1>
             <form>
                 <label htmlFor="">Location
-                    <input id="location" value={location} placeholder="Location"/>
+                    <input id="location" value={location} placeholder="Location" onChange={event=>setLocation(event.target.value)}/>
                 </label>
                 <button>Submit</button>
             </form>
